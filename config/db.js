96df742e1edcf12595fd58e6ebd37c96663fbd59
Ayush Vishwakarma <ayushvish6555@gmail.com>
2023-10-24 00:54:34 +0530

    added mongo uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectToDb = async ()=> {
-    await mongoose.connect("mongodb://127.0.0.1:27017/URLSHORTENER?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1" )
+    await mongoose.connect(process.env.)
         .then((conn)=>{
             console.log(`Connected TO database ${conn.connection.host}`)
         }).catch((e)=>{ 
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import app  from "./app.js";
 const PORT  = process.env.PORT 
- 
- console.log("jjbajsbjasbfjkabsfbasfsa") ;
- 
- 
- 
 app.listen(PORT,  ()=> { 
     console.log(`Server is listening on https://localhost:${PORT}`)
 })
